62f14845505330fc295efe162e6b1ef5f7253d03
Jonathan Buch <jbuch@synyx.de>
2023-09-24 13:36:58 +0200

    see, implement the rest of the mandatory base sbi

--- src/see.rs
+++ src/see.rs
@@ -4,16 +4,18 @@ use std::io::{self, Write};
 use std::ops::{Index, IndexMut};
 
 const SBI_VERSION: (u32, u32) = (1, 0);
+const SBI_IMPL_ID: u32 = 0xFFFFFFFF;
+const SBI_IMPL_VERSION: u32 = 1;
 
 #[allow(dead_code)]
 enum Register {
-    // a0: (Error Code)
+    // a0: in/out (Error Code)
     ARG0 = 10,
-    // a1 (Value)
+    // a1: in/out (Value)
     ARG1 = 11,
-    // FID: a6 (Function ID)
+    // a6: FID (Function ID)
     FID = 16,
-    // EID: a7 (Extension ID)
+    // a7: EID (Extension ID)
     EID = 17,
 }
 
@@ -24,6 +26,7 @@ impl Index<Register> for [u32; 32] {
         &self[idx as usize]
     }
 }
+
 impl IndexMut<Register> for [u32; 32] {
     fn index_mut(&mut self, idx: Register) -> &mut Self::Output {
         &mut self[idx as usize]
@@ -48,15 +51,48 @@ impl From<std::num::TryFromIntError> for Error {
         Error::InvalidParam
     }
 }
+
 impl From<io::Error> for Error {
     fn from(_value: io::Error) -> Self {
         Error::Failed
     }
 }
 
+// Base Extension (EID #0x10)
+
 fn sbi_get_spec_version() -> Result<u32, Error> {
     Ok(SBI_VERSION.0 << 24 + SBI_VERSION.1)
 }
+
+fn sbi_get_sbi_impl_id() -> Result<u32, Error> {
+    Ok(SBI_IMPL_ID)
+}
+
+fn sbi_get_sbi_impl_version() -> Result<u32, Error> {
+    Ok(SBI_IMPL_VERSION)
+}
+
+fn sbi_probe_extension(extension_id: u32) -> Result<u32, Error> {
+    match extension_id {
+        0x01 => Ok(1),
+        0x10 => Ok(1),
+        _ => Ok(0)
+    }
+}
+
+fn sbi_get_mvendorid() -> Result<u32, Error> {
+    Ok(0)
+}
+
+fn sbi_get_marchid() -> Result<u32, Error> {
+    Ok(1)
+}
+
+fn sbi_get_mimpid() -> Result<u32, Error> {
+    Ok(SBI_IMPL_VERSION)
+}
+
+//  Legacy Extensions (EIDs #0x00 - #0x0F)
 
 fn sbi_console_putchar(value: u32) -> Result<u32, Error> {
     let char = [u8::try_from(value)?];
@@ -65,12 +101,37 @@ fn sbi_console_putchar(value: u32) -> Result<u32, Error> {
     Ok(0)
 }
 
-pub fn call(registers: &mut [u32; 32]) {
+// Legacy Extensions have a different calling convention
+fn call_0_1(registers: &mut [u32; 32]) {
+    let func = registers[Register::EID];
+
+    let result = match func {
+        0x01 => sbi_console_putchar(registers[Register::ARG0]),
+        _ => Err(Error::NotSupported),
+    };
+
+    match result {
+        Ok(value) => {
+            registers[Register::ARG0] = value;
+        }
+        Err(error) => {
+            eprintln!("error in syscall: {:?}", func);
+            registers[Register::ARG0] = error as u32;
+        }
+    }
+}
+
+fn call_0_2(registers: &mut [u32; 32]) {
     let func = (registers[Register::EID], registers[Register::FID]);
 
     let result = match func {
         (0x10, 0x0) => sbi_get_spec_version(),
-        (0x01, _) => sbi_console_putchar(registers[Register::ARG0]),
+        (0x10, 0x1) => sbi_get_sbi_impl_id(),
+        (0x10, 0x2) => sbi_get_sbi_impl_version(),
+        (0x10, 0x3) => sbi_probe_extension(registers[Register::ARG0]),
+        (0x10, 0x4) => sbi_get_mvendorid(),
+        (0x10, 0x5) => sbi_get_marchid(),
+        (0x10, 0x6) => sbi_get_mimpid(),
         (_, _) => Err(Error::NotSupported),
     };
 
@@ -82,7 +143,14 @@ pub fn call(registers: &mut [u32; 32]) {
         Err(error) => {
             eprintln!("error in syscall: {:?}", func);
             registers[Register::ARG0] = error as u32;
-            registers[Register::ARG1] = 0;
         }
+    }
+}
+
+pub fn call(registers: &mut [u32; 32]) {
+    if (0x00..=0x0F).contains(&registers[Register::EID]) {
+        call_0_1(registers)
+    } else {
+        call_0_2(registers)
     }
 }
