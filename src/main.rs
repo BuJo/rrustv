2ff674a33b27a7767b8e1c12805b70fc102e36b5
Jonathan Buch <jbuch@synyx.de>
2023-09-25 14:49:49 +0200

    Simplify hart spawning again

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,7 @@ use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let threads = args
-        .get(1)
-        .and_then(|x| x.parse::<usize>().ok())
-        .unwrap_or(1);
-    let (send_finished_thread, receive_finished_thread) = std::sync::mpsc::channel();
+    let threads = args.get(1).and_then(|x| x.parse::<u32>().ok()).unwrap_or(1);
 
     let text = fs::read("target/target.text").expect("no .text");
     let data = fs::read("target/target.data").expect("no .data");
@@ -27,38 +23,21 @@ fn main() {
 
     for id in 0..threads {
         let ram = ram.clone();
-        let send_finished_thread = send_finished_thread.clone();
 
         let handle = thread::spawn(move || {
             eprintln!("[{}] hart spawned", id);
-            let mut m = Hart::new(id as u32, ram);
+            let mut m = Hart::new(id, ram);
             for _ in 0..100 {
                 if !m.tick() {
                     break;
                 }
             }
-
-            send_finished_thread.send(id).unwrap();
         });
 
-        handles.push(Some(handle));
+        handles.push(handle);
     }
 
-    loop {
-        // Check if all threads are finished
-        let num_left = handles.iter().filter(|th| th.is_some()).count();
-        if num_left == 0 {
-            break;
-        }
-
-        // Wait until a thread is finished, then join it
-        let id = receive_finished_thread.recv().unwrap();
-        let join_handle = std::mem::take(&mut handles[id]).unwrap();
-        match join_handle.join() {
-            Ok(_) => eprintln!("[{}] hart joined.", id),
-            Err(e) => eprintln!("[{}] hart joined: {:?}", id, e),
-        }
+    for handle in handles {
+        handle.join().expect("hart failed")
     }
-
-    println!("All hart joined.");
 }
