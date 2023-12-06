cf14460d863e5b6d798933c8e6bf9b2810d5afe5
Jonathan Buch <jbuch@synyx.de>
2023-12-06 11:35:22 +0100

    uart, fix interface
    * use correct base address
    * implement LCR as linux reads from it

--- src/uart8250.rs
+++ src/uart8250.rs
@@ -5,13 +5,24 @@ use std::io::{Read, Write};
 
 pub struct Uart8250 {}
 
+#[allow(unused)]
 impl Uart8250 {
+    const RX: usize = 0; // In: Transmit buffer
+    const IER: usize = 1; // In: Interrupt Enable Register
+    const LCR: usize = 3; // In/Out: Line Control Register
+    const MCR: usize = 4; // In: Modem Control Register
+    const LSR: usize = 5; // Out:  Line Status Register
+    const FCR: usize = 2; // In: FIFO Control Register
+    const DLL: usize = 0; // In: Divisor Latch Low
+    const DLM: usize = 1; // In: Divisor Latch Low
+
     pub fn new() -> Uart8250 {
         Uart8250 {}
     }
 }
 
 impl Device for Uart8250 {
+
     fn write_double(&self, _addr: usize, _val: u64) -> Result<(), Fault> {
         Err(Fault::Unimplemented)
     }
@@ -26,9 +37,12 @@ impl Device for Uart8250 {
 
     fn write_byte(&self, addr: usize, val: u8) -> Result<(), Fault> {
         // Emulating a 8250 / 16550 UART
-        if addr == 0x10000000 {
-            print!("{}", val);
-            io::stdout().flush().unwrap();
+        match addr {
+            Uart8250::RX => {
+                print!("{}", val as char);
+                io::stdout().flush().unwrap();
+            }
+            _ => {}
         }
         Ok(())
     }
@@ -48,14 +62,16 @@ impl Device for Uart8250 {
     fn read_byte(&self, addr: usize) -> Result<u8, Fault> {
         // Emulating a 8250 / 16550 UART
         let have_data: bool = false; // XXX: need a way to detect presence of data in stdin
-        if addr == 0x10000005 {
-            Ok(0x60 | have_data as u8)
-        } else if addr == 0x10000000 && have_data {
-            let mut buffer = [0];
-            io::stdin().read_exact(&mut buffer)?;
-            Ok(buffer[0])
-        } else {
-            Ok(0)
+
+        match addr {
+            Uart8250::LSR if have_data =>  {
+                let mut buffer = [0];
+                io::stdin().read_exact(&mut buffer)?;
+                Ok(buffer[0])
+            }
+            Uart8250::LSR => Ok(0x60 | have_data as u8),
+            Uart8250::LCR => Ok(0b0_0_000_0_11),
+            _ => Ok(0)
         }
     }
 }
